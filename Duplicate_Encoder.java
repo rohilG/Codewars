7c2a1a80834a95f47430afb937525519ce482044
Rohil Gupta <rohilvg@gmail.com>
2017-12-25 17:54:25 -0500

    Duplicate Encoder

---
+++ Duplicate_Encoder.java
@@ -0,0 +1,37 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Duplicate_Encoder {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		String str = input.nextLine();
+		
+		System.out.println(encode(str));
+	}
+
+	static String encode(String word) {
+		word = word.toLowerCase();
+		String[] los = word.split("");
+		String ans = "";
+		
+		for (int i = 0; i < los.length; i++) {
+			System.out.println(los[i]);
+		}
+		
+		for (int i = 0; i < los.length; i++) {
+			if (word.substring(0, i).contains(los[i])){
+				ans += ")";
+			}
+			else if (i<los.length-1 && word.substring(i+1, word.length()).contains(los[i])) {
+				ans += ")";
+			}
+			else {
+				ans += "("; 
+			}
+		}
+		
+	    return ans;
+	  }
+}
